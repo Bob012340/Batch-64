f0147f50cbbae082b64227129eadec9ed27e1f9a
Bob012340 <lseabrook333@gmail.com>
2018-08-07 15:53:38 -0700

    added gdt

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -11,6 +11,9 @@ start:
     call set_up_page_tables
     call enable_paging
 
+    ; load the 64-bit GDT
+    lgdt [gdt64.pointer]
+
     ; print `OK` to screen
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
@@ -144,3 +147,12 @@ p2_table:
 stack_bottom:
     resb 64
 stack_top:
+
+section .rodata
+gdt64:
+    dq 0 ; zero  entry
+.code: equ $ - gdt64 ; new
+    dq (1<<43) | (1<<44) | (1<<47) | (1<<53) ; code segment
+.pointer:
+    dw $ - gdt64 - 1
+    dq gdt64
