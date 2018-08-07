fb1eb3b4706f3f1b55883551a2e48e53305df4b5
Bob012340 <lseabrook333@gmail.com>
2018-08-07 15:58:21 -0700

    long jump to new 64-bit assmebly to print okay

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -13,6 +14,8 @@ start:
 
     ; load the 64-bit GDT
     lgdt [gdt64.pointer]
+    
+    jmp gdt64.code:long_mode_start
 
     ; print `OK` to screen
     mov dword [0xb8000], 0x2f4b2f4f
